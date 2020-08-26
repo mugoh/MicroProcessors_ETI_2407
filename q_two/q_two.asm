7e3f4aa39691cfe6867350e20d1215ac32414a60
mugoh <mugoh.ks@gmail.com>
2020-08-26 17:12:32 +0300

    q2: convert dec/ascii

--- q_two/q_two.asm
+++ q_two/q_two.asm
@@ -81,11 +81,13 @@ ascii_to_dec:
     mov AH, 1
     int 21h
 
+    mov ascii_input, AL
     ; Convert ascii character to dec
+
+    mov DL, 0 ; This is a flag used by the below callee
+    call @get_equivalent_ascii
     
     jmp @exit
-
-    
 
 dec_to_ascii:
     mov AH, 09
@@ -100,7 +102,12 @@ dec_to_ascii:
     cmp CX, 255
     jg illegal_dec
 
-    mov dec_counter, CX
+    ; Mov read value to variable
+    ; CX [CL] will be used for loop
+    mov dec_input, CX
+
+    mov DL, 1 ; Flag used by below callee
+    call @get_equivalent_ascii
     
     jmp @exit
 
@@ -115,16 +122,95 @@ illegal_dec:
     jmp @exit
 
 
+; Prints a tab character
+@print_tab proc
+    mov DL, 09
+    mov AH, 2
+    int 21h
+
+    ret
+
+@print_tab endp
+
+
+; Prints a new line
+@print_new_line proc
+
+        mov DL, 0xa
+        mov AH, 2
+        int 21h
+
+        mov DL, 0xd
+        mov AH, 2
+        int 21h
+
+        ret
+@print_new_line endp
+
+
+; Finds the ASCII equivalent of DECIMAL
+; Loops through all ASCII characters
+; O(n) Time complexity where n is 255
+@get_equivalent_ascii proc
+    
+    mov BX, dec_counter ; For comparison, store addr in 16-bit reg
+    mov CL, char ; ASCII 255 controls loop
+
+    do_it:
+        ; DL = 0 -> ASCII to DEC, DL = 1 -> DEC to ASCII
+        cmp DL, 0
+        je cmp_character
+
+        ; Check if BX matches input
+        ; Yes? Break loop, print matching Char
+        ; No? Try next character
+        cmp BX, dec_input
+        je print_answ
+
+        jmp dec_step ; skip the cmp character part since we are doing DEC to ASCII
+
+        cmp_character:
+            cmp CL, ascii_input
+            je print_answ
+
+        dec_step:
+            dec BX
+
+    loop do_it
+
+    print_answ:
+       call @print_new_line
+       ; Print the character the loop stopped at
+        mov AH, 2
+        mov DL, CL
+        int 21h
+        
+        mov AH, 09
+        mov DX, offset space_eq
+        int 21h
+
+        ; Print its DEC equivalent
+        mov AX, BX
+        call print_num
+        
+        ret
+@get_equivalent_ascii endp
+
 .data
     msg_prompt db 0xa, 0xd, "Choose one option below [e.g 1:] $"
     unknown_prompt db 0xa, 0xd, "That's a strange choice dude/dudelady. GoodBye", 0xa, 0xd, "$"
     choice_prompt dw 0xa, 0xd, "1. ASCII to Decimal", 0xa, 0xd, "2. Decimal to ASCII", 0xa, 0xd, "$"
     choice_in db 0xa, 0xd, "Your choice: ", "$"
     illegal_dec_p db 0xa, 0xd, 0xa, 0xd, "Oopsy! ASCII shouldn't exceed 255D$"
+    space_eq db " == $"
     
     choice db ?
     char db 255d
     dec_counter dw 255d
+
+    ; Stores user inputs
+    dec_input dw ?
+    ascii_input db ?
 
     a_d_prompt db 0xa, 0xd, "Enter ascii character: $"
     d_to_a_prompt db 0xa, 0xd, "Enter decimal value of character: $"
